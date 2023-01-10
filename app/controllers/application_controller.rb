8b7e7a537c2c8917e8657dca871b61e72b7d44fd
pranshujainprotonshub <pranshu.jain@protonshub.in>
2023-01-10 12:03:44 +0530

    audit_observer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   respond_to :json
   include ActionController::MimeResponds
+
+  around_filter :set_logger_username
+
+  def set_logger_username
+    Thread.current["username"] = current_user.login || "guest"
+    yield
+    Thread.current["username"] = nil
+  end
 end
